0fc8f033d8af19d2bfa801af4c714891b353fcdf
Renzo Mischianti <renzo.mischianti@gmail.com>
2022-05-17 08:45:42 +0200

    Add UTF8 support and file length basic control. Issue #9

---
+++ examples/STM32_SPIFlash_enc28j60/STM32_SPIFlash_enc28j60.ino
@@ -0,0 +1,136 @@
+/**
+ * SimpleFTPServer ^1.3.0 on STM32 (need FLASH > 64K)
+ * and ethernet w5500
+ * SPI Flash with Adafruit_SPIFlash and SdFat-Adafruit-Fork library
+ *
+
+#ifndef DEFAULT_FTP_SERVER_NETWORK_TYPE_STM32
+	#define DEFAULT_FTP_SERVER_NETWORK_TYPE_STM32 	NETWORK_ETHERNET_ENC
+	#define DEFAULT_STORAGE_TYPE_STM32 STORAGE_SPIFM
+#endif
+
+ *
+ * @author Renzo Mischianti <www.mischianti.org>
+ * @details https://www.mischianti.org/category/my-libraries/simple-ftp-server/
+ * @version 0.1
+ * @date 2022-03-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <Arduino.h>
+
+#include <EthernetEnc.h>
+
+#include "SdFat.h"
+#include "Adafruit_SPIFlash.h"
+
+#include <SimpleFTPServer.h>
+
+Adafruit_FlashTransport_SPI flashTransport(SS, SPI); // Set CS and SPI interface
+Adafruit_SPIFlash flash(&flashTransport);
+
+// file system object from SdFat
+FatFileSystem fatfs;
+
+#define ETHERNET_CS_PIN PA3
+
+FtpServer ftpSrv;   //set #define FTP_DEBUG in ESP8266FtpServer.h to see ftp verbose on serial
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// Set the static IP address to use if the DHCP fails to assign
+#define MYIPADDR 192,168,1,28
+#define MYIPMASK 255,255,255,0
+#define MYDNS 192,168,1,1
+#define MYGW 192,168,1,1
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+void setup() {
+	  // Initialize serial port and wait for it to open before continuing.
+	  Serial.begin(115200);
+	  while (!Serial) {
+	    delay(100);
+	  }
+	  Serial.println("Adafruit SPI Flash FatFs Full Usage Example");
+
+	  // Initialize flash library and check its chip ID.
+	  if (!flash.begin()) {
+	    Serial.println("Error, failed to initialize flash chip!");
+	    while(1) yield();
+	  }
+
+	  Serial.print("JEDEC ID: "); Serial.println(flash.getJEDECID(), HEX);
+	  Serial.print("Flash size: "); Serial.println(flash.size());
+	  Serial.flush();
+
+	  // First call begin to mount the filesystem.  Check that it returns true
+	  // to make sure the filesystem was mounted.
+	  if (!fatfs.begin(&flash)) {
+	    Serial.println("Error, failed to mount newly formatted filesystem!");
+	    Serial.println("Was the flash chip formatted with the SdFat_format example?");
+	    while(1) yield();
+	  }
+	  Serial.println("Mounted filesystem!");
+	  Serial.println();
+
+
+    // You can use Ethernet.init(pin) to configure the CS pin
+    Ethernet.init(ETHERNET_CS_PIN);
+
+    if (Ethernet.begin(mac)) { // Dynamic IP setup
+        Serial.println(F("DHCP OK!"));
+    }else{
+        Serial.println(F("Failed to configure Ethernet using DHCP"));
+        // Check for Ethernet hardware present
+        if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+          Serial.println(F("Ethernet shield was not found.  Sorry, can't run without hardware. :("));
+          while (true) {
+            delay(1); // do nothing, no point running without Ethernet hardware
+          }
+        }
+        if (Ethernet.linkStatus() == LinkOFF) {
+          Serial.println(F("Ethernet cable is not connected."));
+        }
+
+    	  IPAddress ip(MYIPADDR);
+    	  IPAddress dns(MYDNS);
+    	  IPAddress gw(MYGW);
+    	  IPAddress sn(MYIPMASK);
+    	  Ethernet.begin(mac, ip, dns, gw, sn);
+		  Serial.println("STATIC OK!");
+    }
+    delay(5000);
+
+
+    Serial.print("Local IP : ");
+    Serial.println(Ethernet.localIP());
+    Serial.print("Subnet Mask : ");
+    Serial.println(Ethernet.subnetMask());
+    Serial.print("Gateway IP : ");
+    Serial.println(Ethernet.gatewayIP());
+    Serial.print("DNS Server : ");
+    Serial.println(Ethernet.dnsServerIP());
+
+    Serial.println("Ethernet Successfully Initialized");
+    Serial.println();
+
+
+  // Initialize the FTP server
+  ftpSrv.begin("user","password");
+  Serial.println("Ftp server started!");
+}
+
+void loop()
+{
+  ftpSrv.handleFTP();
+
+  // more processes...
+}
---
+++ examples/esp8266_esp32_LittleFS/esp8266_esp32_LittleFS.ino
@@ -0,0 +1,110 @@
+/*
+ * FtpServer esp8266 and esp32 with LittleFS
+ *
+ * AUTHOR:  Renzo Mischianti
+ *
+ * https://www.mischianti.org/2020/02/08/ftp-server-on-esp8266-and-esp32
+ *
+ */
+
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#include <LittleFS.h>
+#elif defined ESP32
+#include <WiFi.h>
+#include <FS.h>
+#include <LittleFS.h>
+#endif
+
+#include <SimpleFTPServer.h>
+
+const char* ssid = "<YOUR-SSID>";
+const char* password = "<YOUR-PASSWD>";
+
+
+FtpServer ftpSrv;   //set #define FTP_DEBUG in ESP8266FtpServer.h to see ftp verbose on serial
+
+void _callback(FtpOperation ftpOperation, unsigned int freeSpace, unsigned int totalSpace){
+  switch (ftpOperation) {
+    case FTP_CONNECT:
+      Serial.println(F("FTP: Connected!"));
+      break;
+    case FTP_DISCONNECT:
+      Serial.println(F("FTP: Disconnected!"));
+      break;
+    case FTP_FREE_SPACE_CHANGE:
+      Serial.printf("FTP: Free space change, free %u of %u!\n", freeSpace, totalSpace);
+      break;
+    default:
+      break;
+  }
+};
+void _transferCallback(FtpTransferOperation ftpOperation, const char* name, unsigned int transferredSize){
+  switch (ftpOperation) {
+    case FTP_UPLOAD_START:
+      Serial.println(F("FTP: Upload start!"));
+      break;
+    case FTP_UPLOAD:
+      Serial.printf("FTP: Upload of file %s byte %u\n", name, transferredSize);
+      break;
+    case FTP_TRANSFER_STOP:
+      Serial.println(F("FTP: Finish transfer!"));
+      break;
+    case FTP_TRANSFER_ERROR:
+      Serial.println(F("FTP: Transfer error!"));
+      break;
+    default:
+      break;
+  }
+
+  /* FTP_UPLOAD_START = 0,
+   * FTP_UPLOAD = 1,
+   *
+   * FTP_DOWNLOAD_START = 2,
+   * FTP_DOWNLOAD = 3,
+   *
+   * FTP_TRANSFER_STOP = 4,
+   * FTP_DOWNLOAD_STOP = 4,
+   * FTP_UPLOAD_STOP = 4,
+   *
+   * FTP_TRANSFER_ERROR = 5,
+   * FTP_DOWNLOAD_ERROR = 5,
+   * FTP_UPLOAD_ERROR = 5
+   */
+};
+
+void setup(void){
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
+
+  /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
+#ifdef ESP32       //esp32 we send true to format spiffs if cannot mount
+  if (LittleFS.begin(true)) {
+#elif defined ESP8266
+  if (LittleFS.begin()) {
+#endif
+      ftpSrv.setCallback(_callback);
+      ftpSrv.setTransferCallback(_transferCallback);
+
+      Serial.println("LittleFS opened!");
+      ftpSrv.begin("user","password");    //username, password for ftp.   (default 21, 50009 for PASV)
+  }
+}
+void loop(void){
+  ftpSrv.handleFTP();        //make sure in loop you call handleFTP()!!
+}
